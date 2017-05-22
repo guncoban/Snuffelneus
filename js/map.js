e7c11d2d0747b0a052bc10a26b635f9af4ea7e0b
gunvld <gunduzhan_nl@hotmail.com>
2017-05-22 17:45:59 +0200

    Eerste commit

---
+++ js/map.js
@@ -0,0 +1,51 @@
+const googleApiKey = 'AIzaSyCHo5c-LRxqHqogNlDqMGvxA1JKLLp3V2U';
+const script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = `//maps.googleapis.com/maps/api/js?key=${googleApiKey}&callback=initMap`;
+document.body.appendChild(script);
+
+const contentString = 'haha';
+const getMeasurementsString = 'http://145.24.222.50:8000/measurements';
+
+function initMap() {
+    const instance = axios.create({
+        baseURL: 'http://145.24.222.50:8000/api/',
+        timeout: 1000
+    });
+
+    instance.get('/measurements')
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    ;
+
+    const map = new google.maps.Map(
+        document.getElementById('map'),
+        {
+            zoom: 12,
+            center: {
+                lat: 51.929433,
+                lng: 4.488680,
+            },
+        },
+    );
+
+    const infoWindow = new google.maps.InfoWindow({
+        content: contentString,
+    });
+
+    const marker = new google.maps.Marker({
+        position: {
+            lat: 51.929431,
+            lng: 4.488679,
+        },
+        map,
+    });
+
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });
+}
