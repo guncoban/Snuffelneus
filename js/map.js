8bbac421c746b8d9262bb64668974dbcf564d90f
gunvld <gunduzhan_nl@hotmail.com>
2017-05-29 20:55:16 +0200

    Geocoder weggehaald, toch in het server script ingebouwd, want teveel requests per seconde/minuut

--- js/map.js
+++ js/map.js
@@ -6,80 +6,56 @@ document.body.appendChild(script);
 
 var tempContentString;
 
-var geocoder;
 
 function initMap() 
 {
-    geocoder = new google.maps.Geocoder();
     const instance = axios.create
-    ({
-        baseURL: 'http://145.24.222.50:8000/api/',
-        headers: {"Access-Control-Allow-Origin": "*"},
-        timeout: 5000
-    });
+        ({
+            baseURL: 'http://145.24.222.50:8000/api/',
+            headers: { "Access-Control-Allow-Origin": "*" },
+            timeout: 5000
+        });
 
     instance.get('/measurements')
-        .then(function(response) 
+        .then(function (response) 
         {
             var pinsData = response.data;
             var markerArray = [];
             var infoWindowArray = [];
-            for (i = 0; i < pinsData.length;i++)
+            for (i = 0; i < pinsData.length; i++)
             {
                 markerArray.push(new google.maps.Marker
-                ({
-                    position: 
-                    {
-                        lat: pinsData[i].location_latitude,
-                        lng: pinsData[i].location_longitude,
-                    },
-                    map,
-                })
-                );
-                var latlng = {lat: pinsData[i].location_latitude, lng: pinsData[i].location_longitude};
-                geocoder.geocode({'location': latlng}, function(results, status) 
-                {
-                    if (status === 'OK') 
-                    {
-                        if (results[1]) 
-                        {
-                            tempContentString = (results[1].formatted_address) + "Temperature : " + pinsData[i].temperature;
-                        } 
-                        else 
-                        {
-                            tempContentString = "Temperature : " + pinsData[i].temperature;
-                            console.log('No matches found')
-                        }
-                } 
-                else 
-                {
-                    tempContentString = "Temperature : " + pinsData[i].temperature;
-                    console.log('Geocoder failed due to: ' + status);
-                }
-                });
-                if (tempContentString)
-                {
-                    infoWindowArray.push(new google.maps.infoWindow
                     ({
-                        content: tempContentString,
+                        position:
+                        {
+                            lat: pinsData[i].location_latitude,
+                            lng: pinsData[i].location_longitude,
+                        },
+                        map,
                     })
-                    )
-                }
-                markerArray[i].addListener('click', function() 
-                {
-                    infoWindowArray[i].open(map, markerArray[i]);
-                });
+                );
+                var latlng = { lat: pinsData[i].location_latitude, lng: pinsData[i].location_longitude };
+                infoWindowArray.push(new google.maps.infoWindow
+                    ({
+                        content: pinsData[i].location_address,
+                    })
+                )
             }
+            markerArray[i].addListener('click', function () 
+            {
+                infoWindowArray[i].open(map, markerArray[i]);
+            });
             console.log(response);
         })
-        .catch(function(error) {console.log(error); })
-    ;
+        .catch(function (error) { console.log(error); })
+        ;
 
     const map = new google.maps.Map(
         document.getElementById('map'),
         {
             zoom: 12,
-            center: {
+            center:
+            {
                 lat: 51.929433,
                 lng: 4.488680,
             },
@@ -91,16 +67,17 @@ function initMap()
     });
 
     const marker = new google.maps.Marker
-    ({
-        position: 
-        {
-            lat: 51.929431,
-            lng: 4.488679,
-        },
-        map,
-    });
+        ({
+            position:
+            {
+                lat: 51.929431,
+                lng: 4.488679,
+            },
+            map,
+        });
 
-    marker.addListener('click', function() {
+    marker.addListener('click', function ()
+    {
         infoWindow.open(map, marker);
     });
 }
