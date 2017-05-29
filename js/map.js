85ab7b3c812a35adad81e9b9d97f3f46130cf3bb
gunvld <gunduzhan_nl@hotmail.com>
2017-05-30 00:08:25 +0200

    design van map

--- js/map.js
+++ js/map.js
@@ -12,6 +12,7 @@ function initMap()
 {
     var infowindow = new InfoBubble
     ({
+        maxWidth: 150,
         padding: 0,
         borderRadius: 5,
         arrowSize: 10,
@@ -75,6 +76,7 @@ function initMap()
                 lng: 4.488680,
             },
             mapTypeId: 'terrain',
+            styles: [ { "featureType": "administrative.land_parcel", "stylers": [ { "visibility": "off" } ] }, { "featureType": "administrative.neighborhood", "stylers": [ { "visibility": "off" } ] }, { "featureType": "landscape.natural.landcover", "stylers": [ { "visibility": "on" } ] }, { "featureType": "landscape.natural.terrain", "stylers": [ { "visibility": "on" } ] }, { "featureType": "poi", "stylers": [ { "visibility": "off" } ] }, { "featureType": "poi", "elementType": "labels.text", "stylers": [ { "visibility": "off" } ] }, { "featureType": "poi.business", "stylers": [ { "visibility": "off" } ] }, { "featureType": "poi.park", "stylers": [ { "visibility": "on" }, { "weight": 2 } ] }, { "featureType": "poi.park", "elementType": "labels", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road", "elementType": "labels", "stylers": [ { "visibility": "off" } ] }, { "featureType": "road", "elementType": "labels.icon", "stylers": [ { "visibility": "off" } ] }, { "featureType": "transit", "stylers": [ { "visibility": "off" } ] }, { "featureType": "water", "stylers": [ { "visibility": "on" } ] }, { "featureType": "water", "elementType": "labels.text", "stylers": [ { "visibility": "off" } ] } ]
         },
     );
 }
